bc0bca17bc9f4f2eb5b0e63c9f0f6dcd4b173265
Mohammed alagha <modyagha7@gmail.com>
2022-10-11 15:03:24 +0300

    feat: create the proposal model and its relation

---
+++ server/models/proposals.ts
@@ -0,0 +1,29 @@
+import sequelize from "../db/config/connection";
+import DataTypes from "sequelize";
+
+const proposals = sequelize.define('proposals', {
+
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    attachments: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    is_accepted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+
+}
+);
+
+// proposals.belongsTo(freelancers)
+
+export default proposals;
