a11ed7f1de7e96eec772f591a46acf6698f7f854
Ali Al-Shanti <alial-shanti@pop-os.localdomain>
2022-10-12 19:46:10 +0300

    feat: fix the server and install morgan

---
+++ server/models/User.ts
@@ -0,0 +1,29 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../db/config/connection';
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM('freelancer', 'client'),
+    allowNull: false,
+  },
+});
+
+export default User;
