bf2e8a4ef6be21fecc78437bc7381556c0c50d10
alishanti98 <alialshanti90@gmail.com>
2022-10-16 15:30:04 +0300

    feat: create route to search and resolve path dbseed

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express';
+import searchJobs from '../controllers';
 
 const router = express.Router();
 
 router.get('/', (req:Request, res:Response) => {
   res.send('Hello Word');
 });
+
+router.get('/jobs-search', searchJobs);
 
 export default router;
