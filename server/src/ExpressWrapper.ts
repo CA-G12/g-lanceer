29b56c9379fc620cef0a43596c64836f17396ff7
Mohammed alagha <modyagha7@gmail.com>
2022-10-16 14:09:07 +0300

    fix: add else statment to catch in express Wrapper

--- server/src/ExpressWrapper.ts
+++ server/src/ExpressWrapper.ts
@@ -10,8 +10,11 @@ const ExpressWrapper = (fn: ControllerFunction): RequestHandler => async (req, r
   } catch (error: any) { // change the type of error when installing yup
     if (error.name === 'ValidationError') {
       res.status(400).json({ message: error.errors });
-    } else if (error.status) res.status(error.status).json({ message: error.message });
-    else { res.status(500).json({ message: 'Server Error' }); }
+    } else if (error.status) {
+      res.status(error.status).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: 'Server Error' });
+    }
   }
 };
 export default ExpressWrapper;
