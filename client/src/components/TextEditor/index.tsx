790559b20fa47925fd78a2aed829aa9f104210aa
Mohammed alagha <modyagha7@gmail.com>
2022-10-17 09:18:05 +0300

    feat: create the textEditor component props relates #52 close #52

--- client/src/components/TextEditor/index.tsx
+++ client/src/components/TextEditor/index.tsx
@@ -1,11 +1,12 @@
-import { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import './style.css';
 
-function TextEditor() {
-  const [value, setValue] = useState('');
-
+interface Props {
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>
+}
+function TextEditor({ value, setValue }: Props) {
   return (
     <div className="text-editor-container">
       <ReactQuill
