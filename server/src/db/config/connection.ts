ffab2ea0df082e0fab4af757bf41041c905fe9e5
alishanti98 <alialshanti90@gmail.com>
2022-10-17 12:21:41 +0300

    fix: configure ssl to db connection

--- server/src/db/config/connection.ts
+++ server/src/db/config/connection.ts
@@ -7,13 +7,16 @@ const {
 } = process.env;
 
 let url: string | undefined;
-
+let ssl : boolean | object = false;
 switch (NODE_ENV) {
   case 'development':
     url = DEV_DATABASE_URL;
     break;
   case 'production':
     url = DATABASE_URL;
+    ssl = {
+      rejectUnauthorized: false,
+    };
     break;
   case 'test':
     url = TEST_DATABASE_URL;
@@ -23,6 +26,12 @@ switch (NODE_ENV) {
 }
 
 if (!url) throw new Error('NODE_ENV is not set');
-const sequelize = new Sequelize(url, { logging: false, dialect: 'postgres' });
+const sequelize = new Sequelize(url, {
+  logging: false,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl,
+  },
+});
 
 export default sequelize;
