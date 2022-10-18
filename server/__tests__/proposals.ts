34ceac90931324a14d9c2664337e59ae8296c760
Mohammed alagha <modyagha7@gmail.com>
2022-10-18 21:39:40 +0300

    test: create the POST/Poposal api tests

---
+++ server/__tests__/proposals.ts
@@ -0,0 +1,78 @@
+import request from 'supertest';
+import app from '../src/app';
+import insertDB from '../src/db/config/build';
+import sequelize from '../src/db/config/connection';
+
+beforeAll(() => insertDB());
+afterAll(() => sequelize.close());
+
+describe('POST /proposals - /api/v1/proposals Testing all Proposal inputs values', () => {
+  test('respond with json containing validation error /description is empty/ with status of 400', async () => {
+    const response = await request(app)
+      .post('/api/v1/proposals')
+      .send({})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body.message[0]).toBe('description is a required field');
+  });
+
+  test('respond with json containing validation error /description length < 15/ with status of 400', async () => {
+    const response = await request(app)
+      .post('/api/v1/proposals')
+      .send({ description: 'gggggggggg' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body.message[0]).toBe('description must be at least 15 characters');
+  });
+
+  test('respond with json containing validation error /jopID is empty/ with status of 400', async () => {
+    const response = await request(app)
+      .post('/api/v1/proposals')
+      .send({ description: 'lorem lorem lorem lorem' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body.message[0]).toBe('jobId is a required field');
+  });
+
+  test('respond with json containing validation error /jopID is string/ with status of 400', async () => {
+    const response = await request(app)
+      .post('/api/v1/proposals')
+      .send({ description: 'lorem lorem lorem lorem', jobId: 'ID' })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400);
+    // eslint-disable-next-line no-useless-escape
+    expect(response.body.message[0]).toBe('jobId must be a `number` type, but the final value was: `NaN` (cast from the value `\"ID\"`).');
+  });
+
+  test('respond with json containing validation error /attachments is not a url/ with status of 400', async () => {
+    const response = await request(app)
+      .post('/api/v1/proposals')
+      .send({
+        description: 'lorem lorem lorem lorem', jobId: 1, id: 10, attachments: 'ht://placeholder.com',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body.message[0]).toBe('attachments must be a valid URL');
+  });
+
+  test('respond with json containing the created proposal details with status of 200', async () => {
+    const response = await request(app)
+      .post('/api/v1/proposals')
+      .send({
+        description: 'lorem lorem lorem lorem', jobId: 1, id: 10, attachments: 'https://placeholder.com/',
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(response.body.data.id).toBe(10);
+    expect(response.body.data.jobId).toBe(1);
+    expect(response.body.data.description.length).toBeGreaterThanOrEqual(15);
+    expect(response.body.data.isAccepted).toBeFalsy();
+    expect(response.body.data.attachments).toBe('https://placeholder.com/');
+  });
+});
