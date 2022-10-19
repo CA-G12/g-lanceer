116ccb3ce0335d5d7c1130b0002cf99fbc06e7eb
alishanti98 <alialshanti90@gmail.com>
2022-10-19 12:49:52 +0300

    feat: edit proposal job component

--- client/src/components/proposalJob/index.tsx
+++ client/src/components/proposalJob/index.tsx
@@ -4,25 +4,20 @@ import './style.css';
 interface Proposal {
   username: string
   description: string,
-  attachements: string,
+  attachments: string,
 }
 interface Props {
   proposal:Proposal
 }
 
 function ProposalJob({ proposal }: Props) {
-  // when the user is a freelancer, the accept btn appears
-  const btnShow = (value:string) => {
-    if (value === 'freelancerUser') {
-      return 'inlineBlock';
-    }
-    return 'none';
-  };
   return (
     <div className="wrapper-proposal ">
       <div className="content-proposal">
         <h3>{proposal.username}</h3>
         <div className="btn-proposal">
+          {/* when the user is a freelancer, the accept btn appears */}
+          {true && (
           <Button
             style={{
               fontSize: '10px',
@@ -31,16 +26,16 @@ function ProposalJob({ proposal }: Props) {
               paddingRight: '15px',
               backgroundColor: '#1C7925',
               color: '#fff',
-              display: btnShow('freelancerUser'),
             }}
           >
             Accept
           </Button>
+          )}
         </div>
       </div>
       <p>{proposal.description}</p>
       <div>
-        <a className="link-attach" href={proposal.attachements}>
+        <a className="link-attach" href={proposal.attachments}>
           See Attachements
         </a>
       </div>
--- client/src/pages/Client/index.tsx
+++ client/src/pages/Client/index.tsx
@@ -3,13 +3,13 @@ import ProposalJob from '../../components/proposalJob/index';
 interface Proposal {
   username: string
   description: string,
-  attachements: string,
+  attachments: string,
 }
 
 const proposal: Proposal = {
   username: 'Ahmed Safi',
   description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci quis soluta ipsa minima.',
-  attachements: 'https://www.freelancer.com/u/eTranslators?w=f&ngsw-bypass=',
+  attachments: 'https://www.freelancer.com/u/eTranslators?w=f&ngsw-bypass=',
 };
 function Client() {
   return (
