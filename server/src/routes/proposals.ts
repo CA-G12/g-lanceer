d1f74c87b196f0b103a0d488477aaa97bbdf8e41
alishanti98 <alialshanti90@gmail.com>
2022-10-20 12:28:21 +0300

    fix: fix the checkuser option in proposal route

--- server/src/routes/proposals.ts
+++ server/src/routes/proposals.ts
@@ -6,6 +6,6 @@ import { checkUserAuth, passportAuthenticate } from '../middlewares/auth';
 const proposalsRouter = express.Router();
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-proposalsRouter.post('/', passportAuthenticate, checkUserAuth('client'), ExpressWrapper(addProposal));
+proposalsRouter.post('/', passportAuthenticate, checkUserAuth('freelancer'), ExpressWrapper(addProposal));
 
 export default proposalsRouter;
