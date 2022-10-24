8330c80fdd4d595f94612d873da9427c3987c106
Mohammed alagha <modyagha7@gmail.com>
2022-10-24 15:21:38 +0300

    fix: edit the proposal field tests

--- server/__tests__/proposals.ts
+++ server/__tests__/proposals.ts
@@ -78,7 +78,7 @@ const proposalsTests = () => {
     const response = await request(app)
       .post('/api/v1/proposals')
       .send({
-        description: 'lorem lorem lorem lorem', jobId: 1, id: 10, attachments: 'https://placeholder.com/',
+        description: 'lorem lorem lorem lorem', jobId: 1, attachments: 'https://placeholder.com/',
       })
       .set({ Cookie: [`token=${FREELANCER_TOKEN}`] })
       .expect('Content-Type', /json/)
