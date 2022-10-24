909912e327c3b1e83af649143d0e57b097e867bf
shathakh <shathaqudaih17@gmail.com>
2022-10-24 14:19:22 +0300

    fix: edit client router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -5,7 +5,7 @@ import proposalsRouter from './proposals';
 import { passportAuthenticate, checkUserAuth, passportAuth } from '../middlewares/auth';
 import getUserData from '../middlewares/getUserData';
 import ExpressWrapper from '../ExpressWrapper';
-import getClientData from '../controllers/client';
+import clientRouter from './client';
 
 const router = express.Router();
 
@@ -13,7 +13,7 @@ passportAuth(passport);
 router.use('/jobs', jobsRouter);
 router.use('/proposals', proposalsRouter);
 router.get('/user', passportAuthenticate, ExpressWrapper(getUserData));
-router.use('/client', passportAuthenticate, checkUserAuth('client'), ExpressWrapper(getClientData));
+router.use('/client', passportAuthenticate, checkUserAuth('client'), clientRouter);
 router.use('/freelancer', passportAuthenticate, checkUserAuth('freelancer'), (req, res) => {
   res.send('freelancer');
 });
