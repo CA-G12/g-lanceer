213008244481fe27fb6f09f5b4f187cee9fd3d25
naghamabuwarda <nasomabuwarda@gmail.com>
2022-10-24 09:25:30 +0300

    feat: create the context relates #91

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createContext } from 'react';
+import { User } from './interfaces';
+
 import './App.css';
 
+const UserContext = createContext<User | null>(null);
+const user: User = {
+  id: 3,
+  email: 'user@gmail.com',
+  name: 'user',
+  role: 'client',
+};
 const theme = createTheme({
   palette: {
     primary: {
@@ -16,12 +26,15 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div>
-        <nav>navbar</nav>
-        <Outlet />
-        <footer>footer</footer>
+        <UserContext.Provider value={user}>
+          <nav>navbar</nav>
+          <Outlet />
+          <footer>footer</footer>
+        </UserContext.Provider>
       </div>
     </ThemeProvider>
   );
 }
+//   const value = React.useContext(UserContext);
 
 export default App;
