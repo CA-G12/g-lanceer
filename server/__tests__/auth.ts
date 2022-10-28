58f1513e839beac0586f21b1b1047d5cd0e7138f
shathakh <shathaqudaih17@gmail.com>
2022-10-28 11:29:46 +0300

    feat: create tests for signup route relates #125

--- server/__tests__/auth.ts
+++ server/__tests__/auth.ts
@@ -59,6 +59,79 @@ const AuthTests = () => {
       .expect(200);
     expect(response.headers['set-cookie']).toBeTruthy();
   });
+  test('sign up without password and email', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({})
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body.message[0]).toBe('password is a required field');
+  });
+  test('sign up without email', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({ password: '1234585hcf' })
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body.message[0]).toBe('email is a required field');
+  });
+  test('sign up without name', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({ password: '1234585hcf', email: 'shahhd@gmail.com' })
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body.message[0]).toBe('name is a required field');
+  });
+  test('sign up with invalid email', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({ email: '555ahmed', password: '123456788' })
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body.message[0]).toBe('email must be a valid email');
+  });
+  test('sign up successfully ', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'jnekjfhejfk@gmail.com', password: '1234jfbef', name: 'shthaaaadsdswd', role: 'client',
+      })
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(response.body.data).toBe('signed up successfully ');
+  });
+  test('sign up without role ', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'lolo200@gmail.com', password: '1234jfbef', name: 'shthaaaadsdswd',
+      })
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body.message[0]).toBe('role is a required field');
+  });
+  test('response header contains set-cookie header', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'sham@gmail.com', password: '12345678', name: 'shammmghkdhgdd', role: 'client',
+      })
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(response.headers['set-cookie']).toBeTruthy();
+  });
+  test('sign up with used email', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'sham@gmail.com', password: '123456788', name: 'shammmiddbhdu', role: 'client',
+      })
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body.message).toBe('email is already used');
+    expect(response.headers['set-cookie']).toBeUndefined();
+  });
 };
 
 export default AuthTests;
