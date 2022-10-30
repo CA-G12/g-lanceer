e1c2ea44a7475005394474451ed967c61494cff1
Mohammed alagha <modyagha7@gmail.com>
2022-10-30 11:26:52 +0200

    feat: connect login form to login api

--- client/src/helpers/fetchFunction.ts
+++ client/src/helpers/fetchFunction.ts
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
+interface Credentials {
+  email: string
+  password: string
+}
 const getJobs = async (url: string, config: any) => {
   const data = await axios.get(url, config);
   return data;
 };
-export default getJobs;
+const login = async (credentials: Credentials) => {
+  const res = await axios.post('api/v1/auth/login', credentials);
+  return res;
+};
+export { getJobs, login };
--- server/src/controllers/authentication.ts
+++ server/src/controllers/authentication.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
 import { compare } from 'bcrypt';
-import { User } from '../models';
+import { Freelancer, User } from '../models';
 import { loginValidation } from '../validation';
 import { generateToken, serverErrs } from '../helpers';
 
 const login = async (req: Request, res: Response) => {
+  let id;
   const { email, password } = req.body;
   await loginValidation.validate({ email, password });
 
@@ -14,9 +15,14 @@ const login = async (req: Request, res: Response) => {
 
   if (!result) throw serverErrs.BAD_REQUEST('Wrong Email Or Password');
 
-  const { name, role, id } = user;
+  const { name, role } = user;
+  id = user.id;
+  if (user.role === 'freelancer') {
+    const freelancer = await Freelancer.findOne({ where: { userId: user.id } });
+    if (freelancer) id = freelancer?.id;
+  }
   const token = await generateToken({ userID: id, role, name });
   res.cookie('token', token);
-  return { status: 200, msg: 'logged in successfully' };
+  return { status: 200, msg: 'logged in successfully', data: { userID: id, role, name } };
 };
 export default login;
