64682266241620e92794c5ff651aa2ca6c629983
Mohammed alagha <modyagha7@gmail.com>
2022-11-03 11:54:51 +0200

    feat: deploy prepare

--- server/src/app.ts
+++ server/src/app.ts
@@ -26,9 +26,9 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1', router);
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(join(__dirname, '..', 'client', 'build')));
+  app.use(express.static(join(__dirname, '..', '..', 'client', 'build')));
   app.get('*', (req, res) => {
-    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+    res.sendFile(join(__dirname, '..', '..', 'client', 'build', 'index.html'));
   });
 }
 
