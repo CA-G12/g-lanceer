c342fb0f44a0c48f9046013074e2e45a9443f3f1
shathakh <shathaqudaih17@gmail.com>
2022-11-09 18:17:46 +0200

    fix: edit the page of not found error

--- client/src/pages/PageNotFound/index.tsx
+++ client/src/pages/PageNotFound/index.tsx
@@ -1,17 +1,32 @@
 import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+import notFoundError from '../../assets/404.png';
+
 import './style.css';
 
 function PageNotFound() {
   return (
     <div className="error-container">
-      <div className="container">
-        <h1 className="title">404</h1>
-        <p className="description">
-          The page you were looking for could not be found. The page could be
-          removed or you missbled the word while searching for it.
-        </p>
-        <Link to="/">Home</Link>
-      </div>
+      <img src={notFoundError} alt="404 error" className="not-found-err" />
+      <p className="description">
+        The page you were looking for could not be found. The page could be
+        removed or you missbled the word while searching for it.
+      </p>
+      <Link to="/">
+        <Button
+          variant="contained"
+          style={{
+            backgroundColor: '#3A6FE6',
+            color: '#fff',
+            padding: '.6rem 2.4rem',
+            marginTop: '15px',
+          }}
+        >
+          Back Home
+        </Button>
+
+      </Link>
+
     </div>
   );
 }
